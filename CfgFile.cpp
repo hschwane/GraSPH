d02638c87f7a60b36bbd77bb2f89e160d76be9ca
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2016-02-01 19:30:05 +0100

    created files for the CfgFile class for configuration files

---
+++ CfgFile.cpp
@@ -0,0 +1,21 @@
+/*
+ * mpUtils
+ * Config.cpp
+ *
+ * @author: Hendrik Schwanekamp
+ * @mail:   hendrik.schwanekamp@gmx.net
+ *
+ * Implements the Config class, which can read, edit and create "init style" configuration files
+ *
+ * Copyright 2016 Hendrik Schwanekamp
+ *
+ */
+
+
+// includes
+//--------------------
+#include "CfgFile.h"
+//--------------------
+
+// function definitions of the CfgFile class
+//-------------------------------------------------------------------
---
+++ CfgFile.h
@@ -0,0 +1,40 @@
+/*
+ * mpUtils
+ * Config.h
+ *
+ * @author: Hendrik Schwanekamp
+ * @mail:   hendrik.schwanekamp@gmx.net
+ *
+ * Implements the Config class, which can read, edit and create "init style" configuration files
+ *
+ * Copyright 2016 Hendrik Schwanekamp
+ *
+ */
+
+#ifndef MPUTILS_CONFIG_H
+#define MPUTILS_CONFIG_H
+
+// includes
+//--------------------
+
+//--------------------
+
+
+
+/**
+ * class CfgFile
+ * 
+ * usage:
+ * This class manages "init style" configuration files
+ *
+ */
+class CfgFile
+{
+public:
+
+private:
+
+};
+
+
+#endif //MPUTILS_CONFIG_H
