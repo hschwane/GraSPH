1a2ac50201fa13f54512e9a8cee62429a05fb745
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2017-11-16 08:59:05 +0100

    added offset calculation function to graphics

--- src/Graphics/Graphics.h
+++ src/Graphics/Graphics.h
@@ -70,6 +70,18 @@ void inline enableVsync(bool enabled)
         glfwSwapInterval(0);
 }
 
+/** Calculates the byte offset of a given member.
+ * usage:
+ * auto off = offset_of(&MyStruct::my_member);
+ */
+template<typename T, typename TMember>
+int offset_of(TMember T::* field) noexcept
+{
+    // Use 0 instead of nullptr to prohibit a reinterpret_cast of nullptr_t
+    // which throws a compiler error on some compilers.
+    return static_cast<int>(reinterpret_cast<size_t>(&(reinterpret_cast<T*>(0)->*field)));
+}
+
 }}
 
 // include everything useful from the graphics part of the framework
