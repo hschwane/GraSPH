9c7085072c817fd266c62135c3f7fb384c1601a7
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2018-05-29 15:32:49 +0200

    clean up the pressure shader

--- exec/GraSPH/shader/common.glsl
+++ exec/GraSPH/shader/common.glsl
@@ -1,13 +1,5 @@
 #pragma once
 
-// types from the prticle bufffer
-#define POS_TYPE vec4
-#define VEL_TYPE vec4
-#define ACC_TYPE vec4
-#define HYDRO_TYPE vec4
-#define SMLENGTH_TYPE float
-
-
 // buffer bindings
 #define RENDERER_POSITION_BUFFER_BINDING 0
 
@@ -20,7 +12,25 @@
 #define PARTICLE_TIMESTEP_BUFFER_BINDING 7
 #define VERLET_BUFFER_BINDING 8
 
-
 // arrays for rendering data
 #define RENDERER_POSITION_ARRAY 0
 #define RENDERER_MASS_ARRAY 1
+
+// defines for buffer access
+
+// position buffer
+#define POSITION xyz
+#define MASS w
+
+// velocity buffer
+#define VELOCITY xyz
+
+// acceleration buffer
+#define ACCEL xyz
+#define MAXVSIG w
+
+// hydro buffer
+#define DENSITY x
+#define PRESSURE y
+#define SPEED_OF_SOUND z
+#define DRHO_DH w
